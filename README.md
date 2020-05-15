18f9684d093f17812892015cf54df20a690cceee
he <yoo>
2020-05-15 03:01:25 +0200

    To do list added to README

--- README.md
+++ README.md
@@ -3,6 +3,16 @@ Python GUI first testing project
 
 Calculator GUI implementation practice:
 
+To do:
+
+	- "k option" when operation button pressed repeatedly
+	
+	- C button to erease last digit
+	
+	- ANS button to retrieve previous results
+	
+	- Change some globals to function parameters by reference
+
 ![alt text](https://github.com/berbaba/Calculadora/blob/master/Calc_Screenshot_update.png?raw=true " Calculator Screenshot")
 
 <!-- Reference-style:
--- Updates_Description_of_GUI_Picture.py
+++ Updates_Description_of_GUI_Picture.py
@@ -1,16 +1,16 @@
 # Taking Screenshot Using An Tkinter application
 
-import pyautogui   # import PyAutoGUI library       # Required installation pip install pyautogui and  sudo apt-get install scrot
-import tkinter as tk  # import tkinter library
+import pyautogui            # import PyAutoGUI library: Required installation pip install pyautogui
+import tkinter as tk        # and  sudo apt-get install scrot
 
 path_toFile = '/home/bernat/Documents/Estudiant/Python/environments/pildoras/Gràfics/Calculadora/'
 fileName = 'calc_Clear_button_locate.png'
-button_location = pyautogui.locateOnScreen(path_toFile+fileName)     # Locates the image from File in the current screen
-left = button_location.left
-right = button_location.width
-top = button_location.top
-bottom = button_location.height
+button_location = pyautogui.locateOnScreen(path_toFile+fileName) # Locates the image from File in the current screen
+left = button_location.left - 390           # Once the target is located, the shape coordinates are corrected
+right = button_location.width + 420         # manually
+top = button_location.top - 140
+bottom = button_location.height + 580
 # define a method that will call whenever to take the screenshot
 def take():
-    image = pyautogui.screenshot(path_toFile+"Calc_Screenshot_update.png", region = (left-390,top-140,right+420,bottom+580))
+    image = pyautogui.screenshot(path_toFile+"Calc_Screenshot_update.png", region = (left,top,right,bottom))
 take()
